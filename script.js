b06429972e9a2bd1d513a0e7f55f1298da097a4b
mac <ezefavourchiamaka01@gmail.com>
2022-06-22 16:19:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var myName = "Favour Eze"
+console.log(`Hello! I'm ${myName}.`)
+
+var height = "5'9ft"
+console.log(`I'm ${height} tall.`)
+
+var country = "Nigeria"
+console.log(`I am from ${country}`)
+
+alert(`Hi There! My name is ${myName},\n I am ${height} tall and I'm ${country}n.\n Nice to meet you.`)
+
